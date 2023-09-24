a69e1e7e4033a01e201be0f34038a235bc434140
Sam <samantha_malloire@orange.fr>
2023-09-24 13:19:10 +0200

    ajout page entrainement test hiraganas et katakanas

---
+++ data.js
@@ -0,0 +1,8 @@
+var pseudoList = ["guest", "smalloir", "acomet", "chmartin", "gpouzet", "sbeaucie", "cglandus", "ngrenoux", "parinder"];
+
+var img = [
+            "Chitoge.jpg", "Koro.jpg", "FMA-creature.png", "Juuzou.jpg", "Luffy.png", 
+           "Moka.png", "Naruto%20sexy%20jutsu.png", "Pico.jpg", "Rock%20Lee.jpg", "Akame.jpg",
+           "Shou%20Tuker.png", "Yuno.png", "Light%20Yagami.png", "Decim.png", "Soma%20Yukihira.jpg",
+            "Yumeko%20Jabami.jpg", "Son%20Goku.jpg", "Shinichi%20Kudo.jpg", "Haru.png", "Erza%20Scarlet.jpg"
+];
