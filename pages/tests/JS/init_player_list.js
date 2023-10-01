3dde492826db42575217ff1b5676ec7dbbd69813
Sam <samantha_malloire@orange.fr>
2023-10-01 20:10:50 +0200

    photos intra supprimées et selection des joueurs gardée

--- pages/tests/JS/init_player_list.js
+++ pages/tests/JS/init_player_list.js
@@ -1,29 +1,29 @@
 function init_player_list()
 {
+    var j, rand;
     var imgGuest = new Array();
     for (let n = 0; myList[n]; n++)
     {
         players[n] = new Array(3);
         players[n][0] = myList[n];
-        players[n][1] = myList[n] + ".jpg";
         players[n][2] = [0, 0, 0, 0];
-        var rand = Math.floor(Math.random() * img.length);
-        if (myList[n] == pseudoList[0])
+        
+        rand = Math.floor(Math.random() * img.length);
+        j = 0;
+        while (j < imgGuest.length)
         {
-            j = 0;
-            while (j < imgGuest.length)
+            if (imgGuest[j] == img[rand])
             {
-                if (imgGuest[j] == img[rand])
-                {
-                    rand = Math.floor(Math.random() * img.length);
-                    j = 0;
-                }
-                else
-                    j++;
+                rand = Math.floor(Math.random() * img.length);
+                j = 0;
             }
-            imgGuest.push(img[rand]);
-            players[n][0] = img[rand].substring(0, img[rand].length - 4).replace(/%20/g, ' ');
-            players[n][1] = img[rand];
+            else
+                j++;
         }
+        imgGuest.push(img[rand]);
+        
+        if (myList[n] == pseudoList[0])
+            players[n][0] = img[rand].substring(0, img[rand].length - 4).replace(/%20/g, ' ');
+        players[n][1] = img[rand];
     }
 }
